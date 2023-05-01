41db35c98efa1b5e95355bf69a5f23ea23f4d29e
tnaceur <tnaceur@student.1337.ma>
2023-05-01 11:50:03 +0100

    CPP_04

--- CPP_04/ex03/AMateria.hpp
+++ CPP_04/ex03/AMateria.hpp
@@ -2,6 +2,7 @@
 # define AMATERIA_HPP
 
 #include <iostream>
+#include "ICharacter.hpp"
 
 class AMateria
 {
@@ -12,6 +13,46 @@ class AMateria
         std::string const & getType() const; //Returns the materia type
         virtual AMateria* clone() const = 0;
         virtual void use(ICharacter& target);
+};
+
+class	Ice : public AMateria
+{
+    public:
+        Ice ()
+        {
+            this->type = "ice";
+        }
+        AMateria* clone() const
+        {
+            return (new Ice(*this));
+        }
+        void use(ICharacter& target)
+        {
+            std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+        }
+        ~Ice ()
+        {
+        }
+};
+
+class	Cure : public AMateria
+{
+    public:
+        Cure ()
+        {
+            this->type = "cure";
+        }
+        AMateria* clone() const
+        {
+            return (new Cure(*this));
+        }
+        void use(ICharacter& target)
+        {
+            std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+        }
+        ~Cure ()
+        {
+        }
 };
 
 #endif
--- CPP_04/ex03/ICharacter.hpp
+++ CPP_04/ex03/ICharacter.hpp
@@ -1,6 +1,8 @@
 #ifndef ICHARACTER_HPP
 # define ICHARACTER_HPP
 
+#include "AMateria.hpp"
+
 class ICharacter
 {
 public:
@@ -9,6 +11,76 @@ public:
     virtual void equip(AMateria* m) = 0;
     virtual void unequip(int idx) = 0;
     virtual void use(int idx, ICharacter& target) = 0;
+};
+
+class character : public ICharacter
+{
+private:
+    std::string name;
+    AMateria *inventory[4];
+public:
+    character(){
+        this->name = "default";
+        for (int i = 0; i < 4; i++)
+            this->inventory[i] = NULL;
+    }
+    character(const character &copy)
+    {
+        *this = copy;
+    }
+    character &operator=(const character &copy)
+    {
+        this->name = copy.name;
+        for (int i = 0; i < 4; i++)
+        {
+            if (copy.inventory[i] != NULL)
+                this->inventory[i] = copy.inventory[i]->clone();
+            else
+                this->inventory[i] = NULL;
+        }
+        return (*this);
+    }
+    character(std::string name)
+    {
+        this->name = name;
+        for (int i = 0; i < 4; i++)
+            this->inventory[i] = NULL;
+    }
+    ~character(){
+        for (int i = 0; i < 4; i++)
+        {
+            if (this->inventory[i] != NULL)
+                delete this->inventory[i];
+        }
+    }
+    std::string const & getName() const
+    {
+        return (this->name);
+    }
+    void equip(AMateria* m)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (this->inventory[i] == NULL)
+            {
+                this->inventory[i] = m;
+                break;
+            }
+        }
+    }
+    void unequip(int idx)
+    {
+        if (idx < 0 || idx > 3)
+            return ;
+        this->inventory[idx] = NULL;
+    }
+    void use(int idx, ICharacter& target)
+    {
+        if (idx < 0 || idx > 3)
+            return ;
+        if (this->inventory[idx] != NULL)
+            this->inventory[idx]->use(target);
+    }
 };
 
 #endif
