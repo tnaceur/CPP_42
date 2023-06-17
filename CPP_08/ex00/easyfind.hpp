5f2df67a6cc7093893e52db5e9e2a11beb380d8d
tnaceur <tnaceur@student.1337.ma>
2023-06-17 15:09:38 +0100

    CPP_08

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -10,7 +10,7 @@ int easyfind(T& container, int i)
     typename T::iterator result = std::find(container.begin(), container.end(), i);
     if (result != container.end())
     {
-        std::cout << "Found at index: " << std::distance(container.begin(), result) << std::endl;
+        std::cout << i << " Found at index: " << std::distance(container.begin(), result) << std::endl;
         return std::distance(container.begin(), result);
     }
     throw std::out_of_range("Element not found!");
